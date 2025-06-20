dc76bd7b34b6e61c073d19f08dbc699b4cfc960f
NuvosAevum <wanmohamadhanis@users.noreply.github.com>
2025-06-20 22:45:31 +0800

    fix: add alias for @ to resolve blank page on Netlify

--- HanisPlayground/client/vite.config.ts
+++ HanisPlayground/client/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  base: '/',
-  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-}); 
+  plugins: [react()],
+});
